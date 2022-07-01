4d2a9a9e5753aacbcb2215490339fa3e6e7b0d67
root <nick.french0307@gmail.com>
2022-07-01 11:58:24 -0700

    added functionality for spr function which converts a string to a string

--- string.c
+++ string.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * spr - converts string to string 
+ * @s: string argument 
+ * Return: pointer to new string 
+ */
+char *spr(va_list s)
+{
+	int i;
+	char *str;
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	
+	str = malloc(sizeof(*str) * (len + 1));
+	
+	for (i = 0; i < len; i++)
+		str[i] = s[i];
+	
+	return (str);
+}
